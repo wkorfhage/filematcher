ab59764a721594d245953a88af6a80a73e44e698
Willard Korfhage <wrk@lamp.(none)>
2008-04-11 00:14:19 -0500

    First check in. Project just created.

---
+++ urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^filematcher/', include('filematcher.foo.urls')),
+
+    # Uncomment this for admin:
+#     (r'^admin/', include('django.contrib.admin.urls')),
+)
