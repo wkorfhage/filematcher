223284571577579eec8197a5230de51cfe5b4c85
Willard Korfhage <wrk@lamp.(none)>
2008-04-11 00:49:08 -0500

    Created models, added a little data.

--- matcher/models.py
+++ matcher/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class HashType(models.Model):
+    idHashType = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=100)
+    def __unicode__(self):
+	return self.description
+
+    class Admin:
+	pass
+
+class FileInstance(models.Model):
+    idFile = models.AutoField(primary_key=True)
+    hash = models.CharField(max_length=128)
+    fileLength = models.IntegerField()
+    machine = models.CharField(max_length = 20)
+    fullPath = models.CharField(max_length = 300)
+    fileName = models.CharField(max_length = 100)
+    idHashType = models.ForeignKey(HashType)
+    def __unicode__(self):
+	return self.fileName + " on " + self.machine
+
+    class Admin:
+	pass
--- urls.py
+++ urls.py
@@ -5,5 +5,5 @@ urlpatterns = patterns('',
     # (r'^filematcher/', include('filematcher.foo.urls')),
 
     # Uncomment this for admin:
-#     (r'^admin/', include('django.contrib.admin.urls')),
+     (r'^admin/', include('django.contrib.admin.urls')),
 )
